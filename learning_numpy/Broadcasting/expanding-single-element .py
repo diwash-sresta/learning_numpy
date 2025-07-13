01dde2cf8209723fdd2de2d8a4ebeca1ff563662
Diwash <diwashshrestha108@gmail.com>
2025-07-13 14:59:04 +0545

    Broadcasting & Vectorization

---
+++ learning_numpy/Broadcasting/expanding-single-element .py
@@ -0,0 +1,7 @@
+import numpy as np;
+
+matrix = np.array([[10,20,30],[1,2,3]])
+vector = np.array([5])
+
+result = matrix + vector
+print(result)
---
+++ learning_numpy/Vectorization/addition.py
@@ -0,0 +1,7 @@
+import numpy as np;
+
+arr = np.array([1,2,3])
+arr_2 = np.array([4,5,6])
+
+result = arr + arr_2
+print(result)
